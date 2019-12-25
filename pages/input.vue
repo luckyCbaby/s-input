5e80f6c51961d06cd94094e2ffe8cfaf52448256
shijie <shij@ibuaa.com>
2019-12-25 09:06:09 +0800

    增加使用示例

---
+++ pages/input.vue
@@ -0,0 +1,33 @@
+<template>
+    <view class="container">
+        <uni-s-input label="名字" placeholder="请输入姓名" id="name" @inputValue="getInputValue"></uni-s-input>
+        <uni-s-input label="性别" placeholder="请输入性别" id="sex" @inputValue="getInputValue"></uni-s-input>
+        <uni-s-input label="年龄" placeholder="请输入年龄" id="age" @inputValue="getInputValue"></uni-s-input>
+    </view>
+</template>
+
+<script>
+import UniSInput from '../components/s-input/uni-s-input.vue'
+export default {
+    components: {
+        UniSInput
+    },
+    data () {
+        return {
+            formData: {
+                name: '',
+                sex: '',
+                age: ''
+            }
+        }
+    },
+    methods: {
+        getInputValue (e) {  // 根据指定id获取相应input的值
+            console.log(e)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
